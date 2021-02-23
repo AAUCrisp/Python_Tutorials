82ea8b7f965c52cf4a1112e734f908cb701e65c8
holmdane <57073689+holmdane@users.noreply.github.com>
2021-02-23 15:32:03 +0100

    damn son

--- Anders Holm/Intro_exercise_02/06_exercise.py
+++ Anders Holm/Intro_exercise_02/06_exercise.py
@@ -4,4 +4,7 @@
 #  letter, and at least one number. Your function should return true if the
 #password passed to it as its only parameter is good. Otherwise it should 
 # return false. Include a main program that reads a password from the user
-#  and reports whether or not it is good. 
+#  and reports whether or not it is good.
+
+def check_pswd():
+    
